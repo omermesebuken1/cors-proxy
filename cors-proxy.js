a4eb9c3e324fd96347ab4aebc28d6d5f525df45c
omer <omermesebuken1@gmail.com>
2025-05-02 21:42:23 +0300

    commitler falan

--- cors-proxy.js
+++ cors-proxy.js
@@ -4,10 +4,12 @@ const request = require("request");
 
 const app = express();
 app.use(cors());
+app.options('/proxy', cors());
 
 app.get("/proxy", (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).send("Missing URL param");
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   request({ url, encoding: null }).on("error", (err) => {
     res.status(500).send("Request error");
