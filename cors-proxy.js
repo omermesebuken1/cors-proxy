d0fbdbc088e1eba790d7791398acd8bb6082067a
omer <omermesebuken1@gmail.com>
2025-05-02 20:49:13 +0300

    initial commit

---
+++ cors-proxy.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const request = require("request");
+
+const app = express();
+app.use(cors());
+
+app.get("/proxy", (req, res) => {
+  const url = req.query.url;
+  if (!url) return res.status(400).send("Missing URL param");
+
+  request({ url, encoding: null }).on("error", (err) => {
+    res.status(500).send("Request error");
+  }).pipe(res);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`CORS proxy running on http://localhost:${PORT}`);
+});
